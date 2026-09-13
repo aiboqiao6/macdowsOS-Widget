1436e75edd30614fd7b4bb1f0371cecf8b2eca80
渤乔 艾 <aiboqiao250@outlook.com>
2026-09-13 22:16:56 +0800

    update

--- macdowsOS Widget/build-vulkan.disabled/.qt/QtDeployTargets-Debug.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_macdowsOSWidget_FILE D:/DEV/macdowsOS Widgets/macdowsOS Widget/macdowsOS Widget/build-vulkan/Debug/macdowsOSWidget.exe)
-set(__QT_DEPLOY_TARGET_macdowsOSWidget_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_macdowsOSWidget_RUNTIME_DLLS C:/Qt/6.11.1/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.11.1/msvc2022_64/bin/Qt6Networkd.dll;C:/Qt/6.11.1/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.11.1/msvc2022_64/bin/Qt6Cored.dll)
